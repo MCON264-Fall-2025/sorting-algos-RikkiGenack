22c8c020b81eafa30b05d3598c392af74a097f9a
RikkiGenack <rgenack@student.touro.edu>
2025-12-30 13:05:26 -0500

    Finished Thank you!
    I had AI explain the steps to me without giving me the code.

--- src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
+++ src/main/java/edu/touro/mcon264/sorting/InsertionSort.java
@@ -7,5 +7,16 @@ public class InsertionSort implements Sorter {
     @Override
     public <T> void sort(T[] a, Comparator<? super T> comp) {
         // TODO: implement insertion sort
+        for(int i =1; i<a.length; i++){
+            int j = i-1;
+            T currElement = a[i];
+            while(j!=-1 && comp.compare(a[j], currElement)>0){//left elem is bigger
+                a[j+1] =a[j];
+                j--;
+            }
+            a[j+1]=currElement;//insert into empy spot
+        }
     }
-}
+
+    }
+
